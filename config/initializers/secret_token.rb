d9a07207b79e576e1bf9b0f7e3a960f209e4baea
Jairo Vasquez <jairo.vasquez@gmail.com>
2011-02-25 06:57:00 -0500

    Rails proyect created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metkaalu::Application.config.secret_token = '8d7dc94d7de90cc0b1d943e2e8bfc6872f2abb4f80d72e828a58224f76bd491b85f051ba505c71935cb0a10b4a260870022c53253e3abd7dd126a28242ce7558'
